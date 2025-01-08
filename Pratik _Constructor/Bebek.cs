03432b72f9876e224c1dd9bba595b0ce80fbc8dc
bburakbbasol <bburakbbasol@gmail.com>
2025-01-08 12:27:08 +0300

    Add project files.

---
+++ Pratik _Constructor/Bebek.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+namespace Pratik__Constructor 
+{
+	public class Bebek // Bebek sınıfı tanımlanıyor.
+	{
+		public string Isim { get; set; } // Bebek ismi.
+		public string Soyisim { get; set; } // Bebek soyismi.
+		public DateTime DogumTarih { get; set; } // Bebek doğum tarihi.
+
+		public Bebek() // Parametresiz yapıcı metot.
+		{
+			Console.WriteLine("ıngaaaaaaaaaaaaaaaaaaaaaah " + DateTime.Now); // Mesaj ve tarih yazdırır.
+		}
+
+		public Bebek(string isim, string soyisim, DateTime dateTime) // Parametreli yapıcı metot.
+		{
+			Isim = isim; // İsim atanıyor.
+			Soyisim = soyisim; // Soyisim atanıyor.
+			DogumTarih = dateTime; // Doğum tarihi atanıyor.
+			Console.WriteLine("ıngaaaaaaaaaaaaaaaaaaaaaah " + DateTime.Now); // Mesaj ve tarih yazdırır.
+		}
+	}
+}
---
+++ Pratik _Constructor/Program.cs
@@ -0,0 +1,11 @@
+﻿using Pratik__Constructor; // Pratik__Constructor namespace'i kullanılıyor.
+using System;
+
+Bebek bebek1 = new Bebek(); // Parametresiz constructor ile bebek1 oluşturuluyor.
+bebek1.Isim = "Burak"; // Isim özelliği ayarlanıyor.
+bebek1.Soyisim = "Basol"; // Soyisim özelliği ayarlanıyor.
+Console.WriteLine($"{bebek1.Isim} adında {bebek1.Soyisim} soyadında "); // Bebek1 bilgileri yazdırılıyor.
+
+Bebek bebek2 = new Bebek("Ahmet", "Kaya", DateTime.Now); // Parametreli constructor ile bebek2 oluşturuluyor.
+
+Console.WriteLine($"{bebek2.Isim} adında {bebek2.Soyisim} soyadında tarihinde: {bebek2.DogumTarih} doğdum."); // Bebek2 bilgileri yazdırılıyor.
